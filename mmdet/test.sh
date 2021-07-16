40cd3b9758b62931f6cb61e2011bf24100a491a6
gocat-torch <87464163+gocat-torch@users.noreply.github.com>
2021-07-16 18:16:29 +0900

    Update test.sh

--- mmdet/test.sh
+++ mmdet/test.sh
@@ -1,7 +1,3 @@
-# python test_fr_mia.py /home/ychpark/mmdetection/configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712_cocolike_left.py  \
-# /home/ychpark/mmdetection/work_dirs/faster_rcnn_r50_fpn_1x_voc0712_cocolike_left/epoch_24.pth  --mia_meta FR_R50_left_ep24
-
-
 python tools/test.py \
     configs/pascal_voc/faster_rcnn_r101_fpn_1x_voc0712_cocolike_left.py  \
     work_dirs/faster_rcnn_r101_fpn_1x_voc0712_cocolike_left/epoch_1.pth \
