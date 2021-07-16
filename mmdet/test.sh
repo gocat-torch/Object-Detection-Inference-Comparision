6c92da08c7419b214b9ecce87123ec1a8d369f90
gocat-torch <87464163+gocat-torch@users.noreply.github.com>
2021-07-16 18:21:13 +0900

    Update test.sh

--- mmdet/test.sh
+++ mmdet/test.sh
@@ -1,4 +1,4 @@
 python tools/test.py \
-    configs/pascal_voc/faster_rcnn_r101_fpn_1x_voc0712_cocolike_left.py  \
-    work_dirs/faster_rcnn_r101_fpn_1x_voc0712_cocolike_left/epoch_1.pth \
+    configs/pascal_voc/faster_rcnn_r101_fpn_1x_voc0712_cocolike.py  \
+    work_dirs/faster_rcnn_r101_fpn_1x_voc0712_cocolike/epoch_1.pth \
     --eval bbox > infer_faster_rcnn_r101_fpn_epoch1.out
